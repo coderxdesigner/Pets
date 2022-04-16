780ae53ef8ce80da1b6ee3a3f4cf727628d79d43
Rachel <Rachel.Heneault@gmail.com>
2022-04-16 09:09:19 -0400

    Updated class and use of state

--- src/Details.js
+++ src/Details.js
@@ -2,10 +2,16 @@ import { Component } from "react";
 import { withRouter } from "react-router-dom";
 
 class Details extends Component {
-  constructor() {
-    super();
-    this.state = { loading: true };
-  }
+  state = {
+    loading: true,
+    name: "",
+    animal: "",
+    breed: "",
+    city: "",
+    state: "",
+    description: "",
+  };
+
   async componentDidMount() {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
@@ -21,13 +27,18 @@ class Details extends Component {
     );
   }
   render() {
+    if (this.state.loading) {
+      return <h2>Loading...</h2>;
+    }
     const { animal, breed, city, state, description, name } = this.state;
 
     return (
       <div className="details">
         <div>
           <h1>{name}</h1>
-          <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
           <button>Adopt {name}</button>
           <p>{description}</p>
         </div>
