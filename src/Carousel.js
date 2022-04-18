656362f33aee158e1d6a43f3a51097893d7b1c0d
Rachel <Rachel.Heneault@gmail.com>
2022-04-18 07:20:04 -0400

    Finished image carousel

---
+++ src/Carousel.js
@@ -0,0 +1,37 @@
+import { Component } from "react";
+
+class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  };
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            <img
+              key={photo}
+              src={photo}
+              className={index === active ? "active" : ""}
+              data-index={index}
+              onClick={this.handleIndexClick}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+export default Carousel;
